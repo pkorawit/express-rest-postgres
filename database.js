9242145a39da2ba782a384dfcd7b8b7b6eed0a70
Korawit <korawit.p@gmail.com>
2018-10-28 23:34:15 +0700

    Edit db config

--- database.js
+++ database.js
@@ -1,5 +1,5 @@
 const pgp = require('pg-promise')();
-var db = pgp('postgres://mqvvplpgwkwaww:bbe283a5d4ce86d696c0794bfca51f66630f8553225b83bff3880b169880250a@ec2-54-243-147-162.compute-1.amazonaws.com:5432/ddhq6akm6qbbtp?ssl=true');
+var db = pgp(process.env.DATABASE_URL);
 
 function getAllProducts(req, res) {
     db.any('select * from products')
